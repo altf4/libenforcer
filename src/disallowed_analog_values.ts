de45aa35e5ee3b541fa581b9c351e1d45074f1a6
AltF4 <altf4petro@gmail.com>
2023-10-28 16:53:07 -0700

    Update hasDisallowedCStickCoords to use FloatEquals

--- src/disallowed_analog_values.ts
+++ src/disallowed_analog_values.ts
@@ -1,11 +1,11 @@
-import {Coord} from './index';
+import {Coord, FloatEquals} from './index';
 
 export function hasDisallowedCStickCoords(coordinates: Coord[]): boolean {
     for (var coordinate of coordinates) {   
-        if (Math.abs(coordinate.x) == 0.8) {
+        if (FloatEquals(Math.abs(coordinate.x), 0.8)) {
             return true
         }
-        if (Math.abs(coordinate.x) == 0.6625) {
+        if (FloatEquals(Math.abs(coordinate.x), 0.6625)) {
             return true
         }
     }
